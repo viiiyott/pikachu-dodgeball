fc48a8e42efa2a6162164b2d1d30254e306f9cef
viiiyott <79948079+viiiyott@users.noreply.github.com>
2021-03-15 10:20:13 +0900

    Add files via upload

---
+++ src/js/assets_path.js
@@ -0,0 +1,55 @@
+/**
+ * Manages the paths (file locations) of the game assets.
+ */
+'use strict';
+
+export const ASSETS_PATH = {
+  SPRITE_SHEET: '../src/assets/images/sprite_sheet.json',
+  TEXTURES: {},
+  SOUNDS: {},
+};
+
+const TEXTURES = ASSETS_PATH.TEXTURES;
+TEXTURES.PIKACHU = (i, j) => `pikachu/pikachu_${i}_${j}.png`;
+TEXTURES.BALL = (s) => `ball/ball_${s}.png`;
+TEXTURES.NUMBER = (n) => `number/number_${n}.png`;
+
+TEXTURES.SKY_BLUE = 'objects/sky_blue.png';
+TEXTURES.MOUNTAIN = 'objects/mountain.png';
+TEXTURES.GROUND_RED = 'objects/ground_red.png';
+TEXTURES.GROUND_LINE = 'objects/ground_line.png';
+TEXTURES.GROUND_LINE_LEFT_MOST = 'objects/ground_line_leftmost.png';
+TEXTURES.GROUND_LINE_RIGHT_MOST = 'objects/ground_line_rightmost.png';
+TEXTURES.GROUND_YELLOW = 'objects/ground_yellow.png';
+TEXTURES.NET_PILLAR_TOP = 'objects/net_pillar_top.png';
+TEXTURES.NET_PILLAR = 'objects/net_pillar.png';
+TEXTURES.SHADOW = 'objects/shadow.png';
+TEXTURES.BALL_HYPER = 'ball/ball_hyper.png';
+TEXTURES.BALL_TRAIL = 'ball/ball_trail.png';
+TEXTURES.BALL_PUNCH = 'ball/ball_punch.png';
+TEXTURES.CLOUD = 'objects/cloud.png';
+TEXTURES.WAVE = 'objects/wave.png';
+
+TEXTURES.SACHISOFT = 'messages/common/sachisoft.png';
+TEXTURES.READY = 'messages/common/ready.png';
+TEXTURES.GAME_END = 'messages/common/game_end.png';
+
+TEXTURES.MARK = 'messages/ko/mark.png';
+TEXTURES.POKEMON = 'messages/ko/pokemon.png';
+TEXTURES.PIKACHU_VOLLEYBALL = 'messages/ko/pikachu_volleyball.png';
+TEXTURES.FIGHT = 'messages/ko/fight.png';
+TEXTURES.WITH_COMPUTER = 'messages/ko/with_computer.png';
+TEXTURES.WITH_FRIEND = 'messages/ko/with_friend.png';
+TEXTURES.GAME_START = 'messages/ko/game_start.png';
+
+TEXTURES.SITTING_PIKACHU = 'sitting_pikachu.png';
+
+const SOUNDS = ASSETS_PATH.SOUNDS;
+SOUNDS.BGM = '../src/assets/sounds/bgm.mp3';
+SOUNDS.PIPIKACHU = '../src/assets/sounds/WAVE140_1.wav';
+SOUNDS.PIKA = '../src/assets/sounds/WAVE141_1.wav';
+SOUNDS.CHU = '../src/assets/sounds/WAVE142_1.wav';
+SOUNDS.PI = '../src/assets/sounds/WAVE143_1.wav';
+SOUNDS.PIKACHU = '../src/assets/sounds/WAVE144_1.wav';
+SOUNDS.POWERHIT = '../src/assets/sounds/WAVE145_1.wav';
+SOUNDS.BALLTOUCHESGROUND = '../src/assets/sounds/WAVE146_1.wav';
