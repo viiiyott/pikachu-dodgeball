e18f31237526be8a7c3f55ce414cf77314eefdb8
viiiyott <henry11n@naver.com>
2021-03-29 14:15:24 +0900

    Dependency Security Update

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ function setUpInitialUI() {
   // @ts-ignore
   optionsDropdownBtn.disabled = false;*/
 
-  console.log('ver 03.20.3');
+  console.log('ver 03.29.1');
   loader.load(setup); // setup is called after loader finishes loading
   loadingBox.classList.remove('hidden');
 }
